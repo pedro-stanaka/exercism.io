b1769343ab55c7beb7b27c7bdd502237cc30d651
Pedro Tanaka <pedro.stanaka@gmail.com>
2017-01-31 10:22:32 -0200

    GO - Using gofmt to format code (clock.go)

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -4,32 +4,31 @@ import "fmt"
 
 const testVersion = 4
 
-
 type Clock struct {
-	hours, minutes int 
+	hours, minutes int
 }
 
 func New(hour, minute int) Clock {
 
-	var hourInc = 0;
+	var hourInc = 0
 
-	if (minute != 0) {
-		hourInc = (minute / 60);
-		minute = minute % 60;
+	if minute != 0 {
+		hourInc = (minute / 60)
+		minute = minute % 60
 	}
 
-	if (minute < 0) {
-		minute = 60 + minute;
-		hourInc--;
+	if minute < 0 {
+		minute = 60 + minute
+		hourInc--
 	}
 
-	hour += hourInc;
+	hour += hourInc
 
-	if (hour > 23) {
-		hour = hour % 24;
-	} else if (hour < 0) {
-		hour = (-hour) % 24;
-		hour = 24 - hour;
+	if hour > 23 {
+		hour = hour % 24
+	} else if hour < 0 {
+		hour = (-hour) % 24
+		hour = 24 - hour
 	}
 
 	return Clock{hour, minute}
@@ -40,37 +39,37 @@ func (c Clock) String() string {
 }
 
 func (c Clock) Add(minutes int) Clock {
-	
-	var hourInc = 0;
 
-	c.minutes += minutes;
-	hourInc = (c.minutes / 60);
-	c.minutes = c.minutes % 60;
+	var hourInc = 0
 
-	if (c.minutes < 0) {
-		c.minutes = 60 + c.minutes;
-		c.hours--;
-	}
-	
-	c.hours += hourInc;
+	c.minutes += minutes
+	hourInc = (c.minutes / 60)
+	c.minutes = c.minutes % 60
 
-	if (c.hours > 23) {
-		c.hours = c.hours % 24;
-	} else if (c.hours < 0) {
-		c.hours = (-c.hours) % 24;
-		c.hours = 24 - c.hours;
+	if c.minutes < 0 {
+		c.minutes = 60 + c.minutes
+		c.hours--
 	}
 
-	if (c.hours > 23) {
-		c.hours = c.hours % 24;
+	c.hours += hourInc
+
+	if c.hours > 23 {
+		c.hours = c.hours % 24
+	} else if c.hours < 0 {
+		c.hours = (-c.hours) % 24
+		c.hours = 24 - c.hours
 	}
 
-	return c;
+	if c.hours > 23 {
+		c.hours = c.hours % 24
+	}
+
+	return c
 }
 
 func abs(i int) int {
-	if (i < 0) {
-		return -i;
+	if i < 0 {
+		return -i
 	}
-	return i;
+	return i
 }
