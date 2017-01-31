890bde19905e473895df616bc8718e7c4802cf4f
Pedro Tanaka <pedro.stanaka@gmail.com>
2017-01-31 11:33:00 -0200

    Ruby - Hamming - Converting tabs to space (2)

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,23 +1,22 @@
 module BookKeeping
-	VERSION = 3
-end	
+  VERSION = 3
+end 
 
 class Hamming
-	def self.compute(original, compare)
+  def self.compute(original, compare)
 
-		if original.length != compare.length 
-			raise ArgumentError, "Can't calculate the distance for different length strings."
-		end
+    if original.length != compare.length 
+      raise ArgumentError, "Can't calculate the distance for different length strings."
+    end
 
-		return 0 if original == compare
+    return 0 if original == compare
 
-		distance = 0
+    distance = 0
 
-		original.each_char.with_index do |o, i| 
-			distance += 1 if original[i] != compare[i]
-		end
+    original.each_char.with_index do |o, i| 
+      distance += 1 if original[i] != compare[i]
+    end
 
-		distance
-	end
+    distance
+  end
 end
-
