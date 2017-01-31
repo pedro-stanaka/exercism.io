ee0716f33fbf6c1bc92da3b56600f1f3a82263ed
Pedro Tanaka <pedro.stanaka@gmail.com>
2017-01-31 14:16:21 -0200

    Ruby - Hamming - Fixing linter problems with rubocop

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,16 +1,13 @@
 module BookKeeping
   VERSION = 3
-end 
+end
 
 class Hamming
   def self.compute(original, compare)
+    raise ArgumentError unless original.length == compare.length
 
-    if original.length != compare.length 
-      raise ArgumentError, "Can't calculate the distance for different length strings."
+    original.each_char.with_index.count do |_, i|
+      original[i] != compare[i]
     end
-
-    original.each_char.with_index.count do |_, i| 
-     original[i] != compare[i]
-   end
- end
+  end
 end
