a1a085b5cd30c2b48770771c3771d1f5970006e3
Pedro Tanaka <pedro.stanaka@gmail.com>
2017-01-31 11:30:32 -0200

    Ruby - Hamming - SOLVED

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,23 @@
+module BookKeeping
+	VERSION = 3
+end	
+
+class Hamming
+	def self.compute(original, compare)
+
+		if original.length != compare.length 
+			raise ArgumentError, "Can't calculate the distance for different length strings."
+		end
+
+		return 0 if original == compare
+
+		distance = 0
+
+		original.each_char.with_index do |o, i| 
+			distance += 1 if original[i] != compare[i]
+		end
+
+		distance
+	end
+end
+
