96040f24c49da4393e9b085aa377815b3540154b
Pedro Tanaka <pedro.stanaka@gmail.com>
2017-01-31 10:19:59 -0200

    Go - Removing stub comments on clock.go

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,22 +1,13 @@
-// Clock stub file
-
-// To use the right term, this is the package *clause*.
-// You can document general stuff about the package here if you like.
 package clock
 
 import "fmt"
 
-
-// The value of testVersion here must match `targetTestVersion` in the file
-// clock_test.go.
 const testVersion = 4
 
-// Clock API as stub definitions.  No, it doesn't compile yet.
-// More details and hints are in clock_test.go.
 
 type Clock struct {
 	hours, minutes int 
-} // Complete the type definition.  Pick a suitable data type.
+}
 
 func New(hour, minute int) Clock {
 
@@ -73,7 +64,7 @@ func (c Clock) Add(minutes int) Clock {
 	if (c.hours > 23) {
 		c.hours = c.hours % 24;
 	}
-	
+
 	return c;
 }
 
@@ -83,6 +74,3 @@ func abs(i int) int {
 	}
 	return i;
 }
-
-// Remember to delete all of the stub comments.
-// They are just noise, and reviewers will complain.
