22c7a26f724afc5e02e61436edc211c6b545629d
Pedro Tanaka <pedro.stanaka@gmail.com>
2017-01-31 14:11:43 -0200

    Ruby - Hamming - Fixing mixed tabs

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -9,10 +9,8 @@ class Hamming
       raise ArgumentError, "Can't calculate the distance for different length strings."
     end
 
-    return 0 if original == compare
-
     original.each_char.with_index.count do |_, i| 
-	original[i] != compare[i]
-    end
-  end
+     original[i] != compare[i]
+   end
+ end
 end
