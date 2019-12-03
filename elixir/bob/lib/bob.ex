4b97b222bf54456872e5e0c48cd87dd7790e00c7
Pedro Tanaka <pedro.stanaka@gmail.com>
2019-12-03 14:08:06 +0100

    Elixir - Bob - Iteration 2

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,51 +1,27 @@
 defmodule Bob do
   def hey(input) do
-    input = String.trim(input)
-
-    case shouting?(input) do
-      true -> answer_shouting(input)
-      false -> answer_normal_dialogue(input)
+    cond do
+      silence?(input) -> "Fine. Be that way!"
+      question?(input) -> "Sure."
+      shouting_question?(input) -> "Calm down, I know what I am doing!"
+      shouting?(input) -> "Whoa, chill out!"
+      true -> "Whatever."
     end
+  end
+
+  defp silence?(input) do
+    String.trim(input) == ""
+  end
+
+  defp question?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp shouting_question?(input) do
+    question?(input) and shouting?(input)
   end
 
   defp shouting?(input) do
-    upcase_input = String.upcase(input)
-    diff = String.myers_difference(input, upcase_input)
-
-    cond do
-      is_numeric_string(input) ->
-        false
-
-      true ->
-        case Keyword.fetch(diff, :del) do
-          {:ok, _} -> false
-          :error -> true
-        end
-    end
-  end
-
-  def is_numeric_string(input) do
-    clean_input = String.replace(input, ~r/[[:punct:][:blank:]]/, "")
-
-    case Integer.parse(clean_input) do
-      :error -> false
-      {_, sufix} -> String.length(sufix) == 0
-    end
-  end
-
-  defp answer_shouting(input) do
-    cond do
-      input == "" -> "Fine. Be that way!"
-      String.ends_with?(input, "?") -> "Calm down, I know what I'm doing!"
-      true -> "Whoa, chill out!"
-    end
-  end
-
-  defp answer_normal_dialogue(input) do
-    cond do
-      input == "" -> "Fine. Be that way!"
-      String.ends_with?(input, "?") -> "Sure."
-      true -> "Whatever."
-    end
+    input == String.upcase(input)
   end
 end
